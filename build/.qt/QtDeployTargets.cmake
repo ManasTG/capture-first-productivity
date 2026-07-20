bd4cc3ccf0df7414849d77351dbe1696a723f536
Manas Madhukar <manas.jilal@gmail.com>
2026-07-20 23:22:04 +0530

    chore: Removed build and some misc artifacts

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_cpf_FILE /home/krane/Manas/Projects/OpenSource/capture-first-productivity/build/cpf)
-set(__QT_DEPLOY_TARGET_cpf_TYPE EXECUTABLE)
--- build/.qt/cpf_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "cpf")
-set(working_dir "/home/krane/Manas/Projects/OpenSource/capture-first-productivity")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/krane/Manas/Projects/OpenSource/capture-first-productivity/build/.qt/cpf_res.txt")
